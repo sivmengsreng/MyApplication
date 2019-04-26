fc5a020df91baba1de0a1e5dec52ef2aae36499f
sivmeng <1234>
2019-04-26 23:25:06 +0700

    test

---
+++ app/src/main/java/com/sivmeng/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sivmeng.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"hello world ",Toast.LENGTH_LONG).show();
+    }
+}
